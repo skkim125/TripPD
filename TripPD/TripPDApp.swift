44c083c361814d64b5b18bd513f542f83eacf5a1
skkim125 <kthanks125@gmail.com>
2024-09-12 17:57:10 +0900

    Initial Commit

---
+++ TripPD/TripPDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPDApp.swift
+//  TripPD
+//
+//  Created by 김상규 on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TripPDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
