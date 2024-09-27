76c35e99a77de71cf8643f4ac0648e1dfa87f082
skkim125 <kthanks125@gmail.com>
2024-09-27 14:54:29 +0900

    [Feat] Place 모델 수정
    - address 추가 및 생성자 수정

--- TripPD/Model/Travel/Place.swift
+++ TripPD/Model/Travel/Place.swift
@@ -13,15 +13,17 @@ final class Place: Object, ObjectKeyIdentifiable {
     @Persisted(primaryKey: true) var id: ObjectId
     @Persisted(indexed: true) var time: Date // 시간
     @Persisted var name: String // 장소 이름
+    @Persisted var address: String // 장소 주소
     @Persisted var placeMemo: String? // 장소 메모
     @Persisted var lat: Double // 위도
     @Persisted var lon: Double // 경도
     @Persisted var isStar: Bool // 즐겨찾기
     
-    convenience init(time: Date, name: String, placeMemo: String? = nil, lat: Double, lon: Double, isStar: Bool = false) {
+    convenience init(time: Date, name: String, address: String , placeMemo: String? = nil, lat: Double, lon: Double, isStar: Bool = false) {
         self.init()
         self.time = time
         self.name = name
+        self.address = address
         self.placeMemo = placeMemo
         self.lat = lat
         self.lon = lon
