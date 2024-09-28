df93f0bab4d52e6cbfc4efd91ba9188dcef4df22
skkim125 <kthanks125@gmail.com>
2024-09-28 22:57:18 +0900

    [Feat] 문의하기 이메일 변경
    - 문의하기 이메일 변경

--- TripPD/View/User/UserView.swift
+++ TripPD/View/User/UserView.swift
@@ -41,7 +41,7 @@ struct UserView: View {
             .confirmationDialog("", isPresented: $isOpenMailSheet) {
                 Button {
                     // 메일앱이 깔려있으면 가능
-                    EmailController.shared.sendEmail(subject: "제목을 입력해주세요", body: "문의 내용을 입력해주세요", to: "skkim125@icloud.com")
+                    EmailController.shared.sendEmail(subject: "제목을 입력해주세요", body: "문의 내용을 입력해주세요", to: "kthanks125@gmail.com")
                 } label: {
                     Text("이메일로 문의하기")
                 }
