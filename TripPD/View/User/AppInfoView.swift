d41637ec41e1b3f1205d1326f838e5213cf8d8cb
skkim125 <kthanks125@gmail.com>
2024-09-28 17:22:04 +0900

    [Feat] 설정뷰 UI 및 기능 구현
    - UserView: 설정뷰 임시 UI 생성 / 세개의 탭으로 구성
    - AppInfoView: 앱의 정보 및 앱아이콘, 오픈소스라이브러리 버튼 생성 및 구현

---
+++ TripPD/View/User/AppInfoView.swift
@@ -0,0 +1,75 @@
+//
+//  AppInfoView.swift
+//  TripPD
+//
+//  Created by 김상규 on 9/28/24.
+//
+
+import SwiftUI
+import AcknowList
+
+struct AppInfoView: View {
+    @Environment(\.dismiss) var dismiss
+    private let appVersion: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "버전 정보를 불러올 수 없습니다"
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Image("AppIcon_Setting")
+                .resizable()
+                .scaledToFit()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(.mainApp, lineWidth: 5)
+                }
+                .frame(width: 150, height: 150)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+            
+            VStack(alignment: .center, spacing: 5) {
+                Text("Trip PD")
+                    .font(.appFont(20))
+                
+                Text("ver \(appVersion)")
+                    .font(.appFont(15))
+                    .foregroundColor(.secondary)
+            }
+            .padding(.top, 10)
+            
+            Spacer()
+            
+            Button {
+                guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else { return }
+                UIApplication.shared.open(settingsURL)
+            } label: {
+                Text("오픈소스 라이브러리")
+                    .foregroundStyle(.secondary)
+                    .padding(6)
+            }
+            .buttonStyle(.bordered)
+            .frame(width: 280)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .tint(.mainApp)
+            
+            Text("Copyrightⓒ 2024 Trip PD All rights reserved")
+                .font(.caption2)
+                .foregroundColor(.secondary)
+                .padding(.bottom, 100)
+            
+        }
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "chevron.backward")
+                        .bold()
+                }
+                .tint(.mainApp)
+            }
+        }
+    }
+}
+
+#Preview {
+    AppInfoView()
+}
--- TripPD/View/User/UserView.swift
+++ TripPD/View/User/UserView.swift
@@ -6,11 +6,75 @@
 //
 
 import SwiftUI
+import MessageUI
 
 struct UserView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            List(Settings.allCases, id: \.self) { item in
+                NavigationLink {
+                    switch item {
+                    case .pastTrip:
+                        EmptyView()
+                    case .inquiry:
+                        EmptyView()
+                    case .appInfo:
+                        AppInfoView()
+                    }
+                } label: {
+                    Text("\(item.rawValue)")
+                        .font(.appFont(15))
+                }
+                .listStyle(.plain)
+            }
+            .navigationBarTitle(20, 30)
+            .navigationTitle("설정")
+            .navigationBarTitleDisplayMode(.large)
+//            ForEach(Settings.allCases, id: \.self) { item in
+//                switch item {
+//                case .inquiry:
+//                    Button {
+//                        print("dd")
+//                    } label: {
+//                        settingRowView(item)
+//                    }
+//                case .pastTrip:
+//                    NavigationLink {
+//                        EmptyView()
+//                    } label: {
+//                        settingRowView(item)
+//                    }
+//                    
+//                case .appInfo:
+//                    <#code#>
+//                }
+//            }
+        }
     }
+    
+//    @ViewBuilder
+//    func settingRowView(_ setting: Settings) -> some View {
+//        
+//        Text("\(setting.rawValue)")
+//            .foregroundStyle(.mainApp)
+//            .font(.appFont(15))
+//            .padding()
+//            .frame(maxWidth: .infinity, alignment: .center)
+//            .background(.background)
+//            .overlay {
+//                RoundedRectangle(cornerRadius: 12)
+//                    .stroke(.mainApp, lineWidth: 5)
+//            }
+//            .clipShape(RoundedRectangle(cornerRadius: 12))
+//            .padding(.horizontal)
+//    }
+}
+
+enum Settings: String, CaseIterable {
+    case pastTrip = "지나간 여행 보기"
+    case inquiry = "개발자에게 문의하기"
+    case appInfo = "앱 정보"
 }
 
 #Preview {
